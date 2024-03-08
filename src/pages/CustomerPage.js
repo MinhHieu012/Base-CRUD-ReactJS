5bceba6d45882bdf058da4ab9abcaa42981d1c55
MinhHieu012 <0967710509hieu@gmail.com>
2024-03-08 21:11:49 +0700

    upload code

---
+++ src/pages/CustomerPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class CustomerPage extends React.Component{
+    render(){
+        return(
+            <div className="CustomerPage">
+                <h1>Trang Customer</h1>
+            </div>
+        )
+    }
+}
+
+export default CustomerPage;
